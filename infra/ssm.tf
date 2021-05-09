2a5ebe5caa9fd78c5d82121f943c8637ea800ee9
morbi <mathias@eko.ai>
2021-05-09 22:44:28 +0800

    Add ssm parameters to deploy

---
+++ infra/ssm.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "website-bucket" {
+  name = "website/bucket"
+  type = "String"
+  value = aws_s3_bucket.website.bucket
+}
+
+resource "aws_ssm_parameter" "website-cloudfront-id" {
+  name = "website/cloudfront/id"
+  type = "String"
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
