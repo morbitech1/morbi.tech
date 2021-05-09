c26457713c10201aec22786d78b1a8d278399545
morbi <mathias@eko.ai>
2021-05-09 23:01:20 +0800

    Format tf files

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -5,17 +5,17 @@ resource "aws_cloudfront_origin_access_identity" "website" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id = local.s3_origin_id
+    origin_id   = local.s3_origin_id
 
   }
 
-  enabled = true
-  is_ipv6_enabled = true
+  enabled             = true
+  is_ipv6_enabled     = true
   default_root_object = "index.html"
 
 
   aliases = [
-    "morbi.tech"]
+  "morbi.tech"]
 
   default_cache_behavior {
     allowed_methods = [
@@ -25,10 +25,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       "OPTIONS",
       "PATCH",
       "POST",
-      "PUT"]
+    "PUT"]
     cached_methods = [
       "GET",
-      "HEAD"]
+    "HEAD"]
     target_origin_id = local.s3_origin_id
 
     forwarded_values {
@@ -40,9 +40,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
 
     viewer_protocol_policy = "allow-all"
-    min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   price_class = "PriceClass_200"
@@ -54,7 +54,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         "US",
         "CA",
         "GB",
-        "DE"]
+      "DE"]
     }
   }
 
@@ -64,7 +64,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
-    ssl_support_method = "sni-only"
-    acm_certificate_arn = "	arn:aws:acm:us-east-1:292991427042:certificate/b59f8354-feaa-4a8f-8a84-43a46e8f6b60"
+    ssl_support_method             = "sni-only"
+    acm_certificate_arn            = "	arn:aws:acm:us-east-1:292991427042:certificate/b59f8354-feaa-4a8f-8a84-43a46e8f6b60"
   }
 }
