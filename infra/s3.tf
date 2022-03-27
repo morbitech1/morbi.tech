64c3e42dfc3eaed88d76bfa7de0338cfc0e2ae58
Mathias Bøtcher Iversen <morbitech1@gmail.com>
2022-03-27 14:23:11 +0200

    Fixed access denied (#7)
    * Fixed access denied
    
    * corrected sync command to take dist/morbi.tech
    
    * Added correct policy to bucket
    
    * Fixed json for policy
    
    Co-authored-by: mathias <mathias@us2.ai>

--- infra/s3.tf
+++ infra/s3.tf
@@ -7,6 +7,26 @@ resource "aws_s3_bucket" "website" {
   }
 }
 
+resource "aws_s3_bucket_policy" "default" {
+  bucket = aws_s3_bucket.website.id
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+      {
+          "Sid": "2",
+          "Effect": "Allow",
+          "Principal": {
+              "AWS": "${aws_cloudfront_origin_access_identity.website.iam_arn}"
+          },
+          "Action": "s3:*",
+          "Resource": "${aws_s3_bucket.website.arn}"
+      }
+    ]
+}
+EOF
+}
+
 locals {
   s3_origin_id = "morbi.tech"
 }
