f761ad032524edd0e1ae94a1f9bb77ddb0eea01a
mathias <mathias@us2.ai>
2022-03-27 14:20:09 +0200

    Fixed json for policy

--- infra/s3.tf
+++ infra/s3.tf
@@ -11,18 +11,18 @@ resource "aws_s3_bucket_policy" "default" {
   bucket = aws_s3_bucket.website.id
   policy = <<EOF
 {
-"Version": "2008-10-17",
-"Statement": [
-    {
-        "Sid": "2",
-        "Effect": "Allow",
-        "Principal": {
-            "AWS": "${aws_cloudfront_origin_access_identity.website.iam_arn}"
-        },
-        "Action": "s3:*",
-        "Resource": "${aws_s3_bucket.website.arn}""
-    }
-  ]
+  "Version": "2008-10-17",
+  "Statement": [
+      {
+          "Sid": "2",
+          "Effect": "Allow",
+          "Principal": {
+              "AWS": "${aws_cloudfront_origin_access_identity.website.iam_arn}"
+          },
+          "Action": "s3:*",
+          "Resource": "${aws_s3_bucket.website.arn}"
+      }
+    ]
 }
 EOF
 }
